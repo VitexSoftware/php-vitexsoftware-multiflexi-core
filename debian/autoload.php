83479cad327e1bda694d56c58a0047b8f513b7f6
CyberVitexus <info@vitexsoftware.cz>
2026-03-04 23:45:09 +0100

    Fix autoload.php: replace Makefile $$ escaping with plain $ for valid PHP

--- debian/autoload.php
+++ debian/autoload.php
@@ -10,19 +10,19 @@ require_once '/usr/share/php/EaseFluentPDO/autoload.php';
 require_once '/usr/share/php/JsonSchema/autoload.php';
 
 // PSR-4 autoloader for MultiFlexi classes
-spl_autoload_register(function (string $$class): void {
-    $$prefixes = [
+spl_autoload_register(function (string $class): void {
+    $prefixes = [
         'MultiFlexi\\' => '/usr/share/php/MultiFlexi/',
     ];
-    foreach ($$prefixes as $$prefix => $$baseDir) {
-        $$len = strlen($$prefix);
-        if (strncmp($$prefix, $$class, $$len) !== 0) {
+    foreach ($prefixes as $prefix => $baseDir) {
+        $len = strlen($prefix);
+        if (strncmp($prefix, $class, $len) !== 0) {
             continue;
         }
-        $$relativeClass = substr($$class, $$len);
-        $$file = $$baseDir . str_replace('\\', '/', $$relativeClass) . '.php';
-        if (file_exists($$file)) {
-            require $$file;
+        $relativeClass = substr($class, $len);
+        $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
+        if (file_exists($file)) {
+            require $file;
             return;
         }
     }
